62359fd9e80f445417c92a965c41496ba2cbdebd
Mohamed Ahmed <mohamedaahmed654123@gmail.com>
2026-09-07 02:27:37 +0300

    Add post_review function and add_review view/route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     # path for dealer reviews
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
 
-    # path for add a review view
+    # path for add a review
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
